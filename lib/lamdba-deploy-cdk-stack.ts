58fbfad3622cf8202a00615381cb345213830008
Anthony Ikeda <anthony.ikeda@gmail.com>
2024-01-05 11:29:22 -0800

    Seeing if expanding the pipeline will fix the deployment

--- lib/lamdba-deploy-cdk-stack.ts
+++ lib/lamdba-deploy-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 
 export class LamdbaDeployCdkStack extends cdk.Stack {
@@ -8,11 +9,16 @@ export class LamdbaDeployCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'lambda-1',
+      pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('dev-future-tech/lambda-deploy-cdk', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+
+    pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "223945745576", region: "us-east-1"}
+    }))
+    
   }
 }

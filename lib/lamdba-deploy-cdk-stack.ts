4f323556291dbc6efe45a68831754afd1c9bfb01
Anthony Ikeda <anthony.ikeda@gmail.com>
2024-01-05 10:06:52 -0800

    Testing this out

--- lib/lamdba-deploy-cdk-stack.ts
+++ lib/lamdba-deploy-cdk-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+
 
 export class LamdbaDeployCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LamdbaDeployCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'lambda-1',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('anthonyikeda/lambda-a', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
   }
 }
